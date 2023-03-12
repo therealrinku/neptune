7e1a5e9fe976be8bb94c9e8617cfde9fa205e44b
therealrinku <rinkunited2032@gmail.com>
2023-03-12 09:02:56 +0545

    meta headers update

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -5,6 +5,11 @@ export default function Meta() {
     <Head>
       <title>Neptune Mutual - Frontend Assignment</title>
       <meta title="description" content="Frontend assignment for neptune mutual" />
+      <link rel="shortcut icon" href="https://img.icons8.com/office/1x/refresh.png" />
+
+      <meta title="og:title" content="Neptune Mutual - Frontend Assignment" />
+      <meta title="og:description" content="Frontend assignment for neptune mutual" />
+      <meta title="og:image" content="https://img.icons8.com/office/1x/refresh.png" />
     </Head>
   );
 }
